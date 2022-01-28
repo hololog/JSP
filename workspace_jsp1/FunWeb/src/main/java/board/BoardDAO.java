3b6e85681134845051915fd01cb037a3a69228d6
hololog <93326521+hololog@users.noreply.github.com>
2022-01-28 16:51:46 +0900

    Update BoardDAO.java

--- workspace_jsp1/FunWeb/src/main/java/board/BoardDAO.java
+++ workspace_jsp1/FunWeb/src/main/java/board/BoardDAO.java
@@ -3,6 +3,8 @@ package board;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -59,6 +61,40 @@ public class BoardDAO {
 		
 	
 	// 리턴할형 List  getBoardList(int startRow, int pageSize) 메서드 정의
+		public List getBoardList(int startRow, int pageSize) {
+			List boardList=new ArrayList();
+			try {
+				//1,2 디비연결
+				con=getConnection();
+				//3 sql select * from board order by num desc limit startRow-1,pageSize
+				String sql="select * from board order by num desc limit ?,?";
+				pstmt=con.prepareStatement(sql);
+				pstmt.setInt(1, startRow-1);
+				pstmt.setInt(2, pageSize);
+				//4 실행 => 결과저장
+				rs=pstmt.executeQuery();
+				//5 결과 행접근 => BoardDTO객체생성 => set메서드호출 열접근해서 저장
+				while(rs.next()) {
+					//다음행 데이터 있으면 true => 열접근 => BoardDTO 멤버변수 저장
+					BoardDTO bDTO=new BoardDTO();
+					bDTO.setNum(rs.getInt("num"));
+					bDTO.setPass(rs.getString("pass"));
+					bDTO.setName(rs.getString("name"));
+					bDTO.setSubject(rs.getString("subject"));
+					bDTO.setContent(rs.getString("content"));
+					bDTO.setReadcount(rs.getInt("readcount"));
+					bDTO.setDate(rs.getTimestamp("date"));
+					//배열한칸에 순서대로 저장
+					// BoardDTO형을 모든형(Object)으로 잠시 변경해서 저장=> 업캐스팅
+					boardList.add(bDTO);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				dbClose();
+			}
+			return boardList;
+		}//
 		
 	
 }//클래스
